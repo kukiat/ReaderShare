f49ebb02c7ab7026ebf2e88806eed8c206740e44
kukiat <kukiat1996@gmail.com>
2017-11-06 15:45:37 +0700

    backup

--- app/src/main/java/com/example/kukiat/readershare/MainActivity.java
+++ app/src/main/java/com/example/kukiat/readershare/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.ListView;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
@@ -17,28 +16,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ListView mListView;
-    private CustomAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mListView = (ListView) findViewById(R.id.listView);
-
-        fetchData();
-
     }
 
     private void fetchData() {
+
         String url = "http://blog.teamtreehouse.com/api/get_recent_summary/";
-//        String url = "http://itpart.com/android/json/test8records.php";
-        String jsonString ="";
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
@@ -47,30 +38,21 @@ public class MainActivity extends AppCompatActivity {
 
                         try {
                             JSONArray data = response.getJSONArray("posts");
-                            for(int i=0; i<data.length(); i++) {
-                                JSONObject obj = data.getJSONObject(i);
-                                Log.i("xxx", obj.getString("title"));
-                            }
+
                         }catch (JSONException e) {
                             Log.d("error", e.getMessage());
                             e.printStackTrace();
                         }
-
-                        Log.i("info", response.toString());
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError err){
+
                     }
                 });
+
         Volley.newRequestQueue(this).add(jsonObjectRequest);
 
-//        Gson gson = new Gson();
-//        Blog blog = gson.fromJson(jsonString, Blog.class);
-//
-//        mAdapter = new CustomAdapter(this, blog.getPosts());
-//        mListView.setAdapter(mAdapter);
     }
-
 }
