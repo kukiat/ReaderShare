3845367ee6add299628d49a474bda645a0a85efb
kukiat <kukiat1996@gmail.com>
2017-11-14 11:12:51 +0700

    edit signup

--- app/src/main/java/com/example/kukiat/readershare/SignUpActivity.java
+++ app/src/main/java/com/example/kukiat/readershare/SignUpActivity.java
@@ -2,15 +2,92 @@ package com.example.kukiat.readershare;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by kukiat on 11/13/2017 AD.
  */
 
 public class SignUpActivity extends AppCompatActivity{
+    private EditText email;
+    private EditText password;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account_activity);
+        email = findViewById(R.id.createMailField);
+        password = findViewById(R.id.createPassFiled);
+    }
+
+    public void signUpClick(View v) throws JSONException {
+
+        String URL = "https://readershare.herokuapp.com/register";
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("email", email.getText().toString());
+        jsonBody.put("password", password.getText().toString());
+//        User user = new User(email.getText().toString(),password.getText().toString());
+        final String requestBody = jsonBody.toString();
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError err) {
+                        // error
+                        Log.d("Error.Response", "wdwdwd");
+                    }
+                }
+        ) {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    return null;
+                }
+            }
+            @Override
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                String responseString = "";
+                if (response != null) {
+                    responseString = String.valueOf(response.statusCode);
+                    // can get more details such as response.headers
+                }
+                return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/json");
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(postRequest);
     }
 }
